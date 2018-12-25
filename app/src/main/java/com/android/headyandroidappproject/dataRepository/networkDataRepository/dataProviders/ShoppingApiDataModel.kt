e5ac4c869ba463c384f531283be76a9b9a3385cd
Bhargav Akbari <bhargav@buyhatke.com>
2018-12-25 18:16:34 +0530

    bug fixing

--- app/src/main/java/com/android/headyandroidappproject/dataRepository/networkDataRepository/dataProviders/ShoppingApiDataModel.kt
+++ app/src/main/java/com/android/headyandroidappproject/dataRepository/networkDataRepository/dataProviders/ShoppingApiDataModel.kt
@@ -3,11 +3,12 @@ package com.android.headyandroidappproject.dataRepository.networkDataRepository.
 import com.android.headyandroidappproject.AppController
 import com.android.headyandroidappproject.dataRepository.networkDataRepository.apiProviders.ShoppingAPI
 import com.android.headyandroidappproject.pojo.Category
+import com.android.headyandroidappproject.pojo.ShoppingData
 import io.reactivex.Observable
 
 class ShoppingApiDataModel {
 
-    fun getAllShoppingDataList(): Observable<List<Category>> {
+    fun getAllShoppingDataList(): Observable<ShoppingData> {
         val retrofit = AppController.getApplicationInstance().getRetrofitClient()
         val shoppingApi = retrofit.create(ShoppingAPI::class.java)
         return shoppingApi.getShoppingDataList()

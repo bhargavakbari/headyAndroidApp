35e936aae32ad1c2be9cad13c38828713716e367
Bhargav Akbari <bhargav@buyhatke.com>
2018-12-26 12:51:52 +0530

    added few more params

--- app/src/main/java/com/android/headyandroidappproject/pojo/Product.kt
+++ app/src/main/java/com/android/headyandroidappproject/pojo/Product.kt
@@ -3,4 +3,6 @@ package com.android.headyandroidappproject.pojo
 import com.google.gson.annotations.SerializedName
 
 data class Product(@SerializedName("id") val id: Int,
-                   @SerializedName("view_count") val viewCount: Int = 0)
+                   @SerializedName("view_count") val viewCount: Int = 0,
+                   @SerializedName("order_count") val orderCount: Int = 0,
+                   @SerializedName("shares") val sharesCount: Int = 0)

c2f19a7dc414563e9850ca8cb23505e17dbdac2f
Bhargav Akbari <bhargav@buyhatke.com>
2018-12-26 14:38:47 +0530

    product listing activity added

---
+++ app/src/main/java/com/android/headyandroidappproject/ui/activities/ProductListingActivity.kt
@@ -0,0 +1,79 @@
+package com.android.headyandroidappproject.ui.activities
+
+import android.arch.lifecycle.Observer
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import com.android.headyandroidappproject.R
+import com.android.headyandroidappproject.adapters.GenericAdapter
+import com.android.headyandroidappproject.dataRepository.localDataBase.dataBase.AppDataBase
+import com.android.headyandroidappproject.dataRepository.localDataBase.entities.ProductEntity
+import com.android.headyandroidappproject.listener.RecyclerViewClickListener
+import com.android.headyandroidappproject.utility.Constants.IntentParameter.Companion.CAT_ID
+import com.android.headyandroidappproject.utility.SpacesItemDecoration
+import com.android.headyandroidappproject.viewHolders.ProductItemViewHolder
+import kotlinx.android.synthetic.main.activity_product_listing.*
+import kotlinx.android.synthetic.main.progress_layout.*
+
+class ProductListingActivity : AppCompatActivity(), RecyclerViewClickListener {
+
+    val TAG = "ProductListingActivity"
+    val SPACE = 10
+    var mDb: AppDataBase? = null
+    var catId = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_listing)
+        configRecyclerView()
+        readIntentData()
+        mDb = AppDataBase.getInstance(this)
+        getAllCatDataFromLocalDB()
+    }
+
+    private fun readIntentData() {
+        catId = intent.getIntExtra(CAT_ID, 0)
+    }
+
+    private fun getAllCatDataFromLocalDB() {
+        showProgressBar()
+        val productDataListObserver = Observer<List<ProductEntity>> { productDataList ->
+            hideProgressBar()
+            setAdapter(productDataList)
+            Log.e(TAG, "all cat --->  " + productDataList?.size);
+        }
+        mDb?.getProductDao()?.getAllProductForParticularCategory(catId)?.observe(this, productDataListObserver)
+    }
+
+    private fun setAdapter(categoryDataList: List<ProductEntity>?) {
+        productListingRecyclerView?.let {
+            var adapter = GenericAdapter(categoryDataList as List<Any>, this, R.layout.item_product
+                    , ProductItemViewHolder::class.java.canonicalName, this)
+            productListingRecyclerView?.adapter = adapter
+        }
+    }
+
+
+    private fun configRecyclerView() {
+        productListingRecyclerView.addItemDecoration(SpacesItemDecoration(SpacesItemDecoration.CAT_TAG, SPACE))
+        productListingRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+        }
+    }
+
+    override fun onItemClicked(view: View, position: Int, any: Any) {
+
+    }
+
+    private fun showProgressBar() {
+        progressbarBaseLayout.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressbarBaseLayout.visibility = View.GONE
+    }
+}
---
+++ app/src/main/java/com/android/headyandroidappproject/viewHolders/ProductItemViewHolder.kt
@@ -0,0 +1,33 @@
+package com.android.headyandroidappproject.viewHolders
+
+import android.content.Context
+import android.view.View
+import com.android.headyandroidappproject.dataRepository.localDataBase.entities.ProductEntity
+import com.android.headyandroidappproject.listener.RecyclerViewClickListener
+import kotlinx.android.synthetic.main.item_product.view.*
+import java.util.*
+
+class ProductItemViewHolder(view: View, val mContext: Context) : BaseViewHolder(view, mContext) {
+
+    var clickListener: RecyclerViewClickListener? = null
+    var tvProductName = view.tvProductName
+    var productNameCardView = view.productNameCardView
+    var productNameList = ArrayList<ProductEntity>()
+
+    override fun initView() {
+        productNameCardView.setOnClickListener { clickListener!!.onItemClicked(productNameCardView, adapterPosition, productNameList!![adapterPosition].productId) }
+    }
+
+    override fun initDataForRecyclerView(items: List<Any>) {
+        productNameList = items as ArrayList<ProductEntity>
+    }
+
+    override fun itemClickListenerCallBack(clickListener: RecyclerViewClickListener?) {
+        this.clickListener = clickListener
+    }
+
+    override fun currentItemPosition(position: Int) {
+        super.currentItemPosition(position)
+        tvProductName.text = productNameList!![adapterPosition].productName
+    }
+}

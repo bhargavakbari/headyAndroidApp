bac61cf58d5831eb6f2e6a763ee404ab56858822
Bhargav Akbari <bhargav@buyhatke.com>
2018-12-26 12:53:37 +0530

    minor bug fixing

--- app/src/main/java/com/android/headyandroidappproject/pojo/Ranking.kt
+++ app/src/main/java/com/android/headyandroidappproject/pojo/Ranking.kt
@@ -3,4 +3,4 @@ package com.android.headyandroidappproject.pojo
 import com.google.gson.annotations.SerializedName
 
 data class Ranking(@SerializedName("ranking") val ranking: String = "",
-                   @SerializedName("products") val products: List<ProductsItem>?)
+                   @SerializedName("products") val products: List<Product>?)
